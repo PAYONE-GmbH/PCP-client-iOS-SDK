5ed4f23f0cc1f22890bd46d775474cf02dffbb79
Manuel Schulze <freelance@manuelschulze.com>
2024-08-08 15:31:37 +0200

    ADD base swiftlint configuration

--- Package.swift
+++ Package.swift
@@ -9,15 +9,22 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PCPClient",
-            targets: ["PCPClient"]),
+            targets: ["PCPClient"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PCPClient"),
+            name: "PCPClient",
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+        ),
         .testTarget(
             name: "PCPClientTests",
-            dependencies: ["PCPClient"]),
+            dependencies: ["PCPClient"]
+        ),
     ]
 )
--- Sources/PCPClient/PCPClient.swift
+++ Sources/PCPClient/PCPClient.swift
@@ -1,2 +1,7 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+// This file is part of the PCPClient iOS SDK.
+// Copyright © 2024 PAYONE GmbH. All rights reserved.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
