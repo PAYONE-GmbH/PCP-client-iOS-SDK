b1369efd88f1f4fae015bb90aca9753a171e54c6
Manuel Schulze <freelance@manuelschulze.com>
2024-08-26 19:59:22 +0200

    ADD required types for fingerprint tokenizer

---
+++ Sources/PCPClient/FingerprintTokenizer/FingerprintError.swift
@@ -0,0 +1,14 @@
+//
+// This file is part of the PCPClient iOS SDK.
+// Copyright © 2024 PAYONE GmbH. All rights reserved.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+import Foundation
+
+public enum FingerprintError: Error {
+    case scriptError(error: Error)
+    case undefined
+}
---
+++ Sources/PCPClient/FingerprintTokenizer/PCPEnvironment.swift
@@ -0,0 +1,14 @@
+//
+// This file is part of the PCPClient iOS SDK.
+// Copyright © 2024 PAYONE GmbH. All rights reserved.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+import Foundation
+
+public enum PCPEnvironment: String {
+    case test = "t"
+    case production = "p"
+}
