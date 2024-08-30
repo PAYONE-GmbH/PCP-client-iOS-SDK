8c831958d11b61639cf992c7515023a905e5d8bd
Manuel Schulze <freelance@manuelschulze.com>
2024-08-30 16:48:22 +0200

    REMOVE swiftlint as a dependecy

--- Package.swift
+++ Package.swift
@@ -17,9 +17,6 @@ let package = Package(
             targets: ["PCPClientBridge"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
