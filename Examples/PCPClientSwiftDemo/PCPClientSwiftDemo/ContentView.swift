afd2f61d7326c133cc348a85226199042d25bb70
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2025-09-26 07:48:21 +0200

    Revert "feat: init example PayPal payment integration into demo app"
    This reverts commit 1068e01a1e1164ee84b306fa3d5a83bc02a847b3.

--- Examples/PCPClientSwiftDemo/PCPClientSwiftDemo/ContentView.swift
+++ Examples/PCPClientSwiftDemo/PCPClientSwiftDemo/ContentView.swift
@@ -40,12 +40,6 @@ struct ContentView: View {
         Text("Fingerprint Token:")
         Text(fingerprintToken)
         NavigationLink(destination: creditcardTokenizer) { Text("Go to Creditcard Tokenizer") }
-
-        NavigationLink(destination: PayPalCardView()) {
-          Text("PayPal Card Payment")
-            .padding(.vertical, 8)
-        }
-
         Spacer()
 
         if shouldShowApplePay {
@@ -123,8 +117,7 @@ struct ContentView: View {
     CreditcardTokenizerView(
       tokenizerUrl: URL(string: "YOUR_URL")!,
       config: CreditcardTokenizerConfig(
-        iframeConfig: IframeConfig(
-          iframeWrapperId: "payment-IFrame", height: Double(400), width: Double(400)),
+        iframeConfig: IframeConfig(iframeWrapperId: "payment-IFrame", height: Double(400), width: Double(400)),
         uiConfig: UIConfig(
           formBgColor: "#fff",
           fieldBgColor: "#f9f9f9",
